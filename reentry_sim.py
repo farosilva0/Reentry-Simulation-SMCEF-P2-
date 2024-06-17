c78439ea145175ec48eae7a91086512eed939a74
Garção <j.garcao@campus.fct.unl.pt>
2024-06-17 19:25:39 +0100

    parachutes drag acceleration

--- reentry_sim.py
+++ reentry_sim.py
@@ -20,13 +20,21 @@ CAPSULE_SURFACE_AREA = 4 * np.pi  # surface considered for drag coefficient (m^2
 CAPSULE_DRAG_COEFFICIENT = 1.2  # drag coefficient
 CAPSULE_LIFT_COEFFICIENT = 1.0  # lift coefficient
 
+# Parachute parameters
+PARACHUTE_SURFACE_AREA = 301 # surface considered for parachute's drag coefficient (m^2)
+PARACHUTE_DRAG_COEFFICIENT = 1.0  # parachute's drag coefficient
+ALTITUDE_BOUNDARY = 10**6  # boundary altitude for deployment of the parachutes (m)
+VELOCITY_BOUNDARY = 10**2  # boundary velocity for deployment of the parachutes (m/s)
+
 # Physical constants
 G_M = 6.67430e-11 * 5.972e24    # G (Gravitational constant) * Earth mass, m^3 kg^-1 s^-2
 RADIUS_EARTH = 6.371e6          # Earth radius (m)
 
 def get_air_density_cubic_spline(altitude):
     f = CubicSpline(altitude_values, air_density_values, bc_type='natural')
-    return f(altitude)
+    result = f(altitude)
+
+    return 0.0 if result < 0.0 else result
 
 def get_air_density(y):
     '''returns the air density at a given altitude'''
@@ -42,7 +50,17 @@ def drag_acceleration(vx, vy, v, y, A = CAPSULE_SURFACE_AREA, Cd = CAPSULE_DRAG_
     '''calculates the drag acceleration on the object, given its velocity and altitude.
         By default, the drag force is calculated for the capsule.
         Drag force always opposes the movement direction, so it is negative.'''
-    Fd = -0.5 * Cd * A * get_air_density(y) * v**2  #
+    Fd = -0.5 * Cd * A * get_air_density_cubic_spline(y) * v**2
+    ax = Fd * vx / CAPSULE_MASS
+    ay = Fd * vy / CAPSULE_MASS
+    return ax, ay
+
+
+def drag_acceleration_parachute(vx, vy, v, y, A = PARACHUTE_SURFACE_AREA, Cd = PARACHUTE_DRAG_COEFFICIENT):
+    '''calculates the drag acceleration on the object after the parachutes are deployed, given its velocity and altitude.
+        The mass of the parachutes is despised.
+        Drag force always opposes the movement direction, so it is negative.'''
+    Fd = -0.5 * Cd * A * get_air_density_cubic_spline(y) * v**2
     ax = Fd * vx / CAPSULE_MASS
     ay = Fd * vy / CAPSULE_MASS
     return ax, ay
@@ -52,8 +70,8 @@ def lift_acceleration(vx, vy, v, y, A = CAPSULE_SURFACE_AREA, Cl = CAPSULE_LIFT_
     '''calculates the lift acceleration on the object, given its velocity and altitude.
         By default, the lift force is calculated for the capsule.
         Lift force is perpendicular to the movement direction, so it is calculated using the velocity components.'''
-    Fl = 0.5 * Cl * A * get_air_density(y) * v**2
-    ax = Fl * vy / (CAPSULE_MASS * v)   
+    Fl = 0.5 * Cl * A * get_air_density_cubic_spline(y) * v**2
+    ax = Fl * vy / (CAPSULE_MASS * v)
     ay = -Fl * vx / (CAPSULE_MASS * v)  # Lift force oposes the fall (y component)
     return ax, ay
 
@@ -61,22 +79,26 @@ def lift_acceleration(vx, vy, v, y, A = CAPSULE_SURFACE_AREA, Cl = CAPSULE_LIFT_
 def gravity_acceleration(x, y):
     '''calculates the acceleration due to gravity at a given position (x, y)
         returns the acceleration components gx and gy. '''
-    r = np.sqrt(x**2 + y**2)    # distance from the center of the Earth
-    g = - G_M / r**2            # gravity acceleration pointing to the center of the Earth (negative because it goes down) 
+    r = y + RADIUS_EARTH        # distance from the center of the Earth
+    g = - G_M / r**2            # gravity acceleration pointing to the center of the Earth (negative because it goes down)
     gx = g * (x / r)           
     gy = g * (y / r)
     return gx, gy
 
 
-def total_acceleration(x, y, vx, vy):
+def total_acceleration(x, y, vx, vy, deployment):
     '''calculates the total acceleration on the capsule'''
     v = np.sqrt(vx**2 + vy**2) # velocity in the movement's direction
-    dx, dy = drag_acceleration(vx, vy, v, y)
-    lx, ly = lift_acceleration(vx, vy, v, y)
-    # TODO: calcular as 2 acelarações em conjunto?? ou tem de ser separado??
     gx, gy = gravity_acceleration(x, y)
-    # TODO: somar as acelerações todas
-    return gx, gy
+    dx, dy = drag_acceleration(vx, vy, v, y)
+    if deployment: # case where the parachutes are deployed and there is no lift resistance
+        resistance_x, resistance_y = drag_acceleration_parachute(vx, vy, v, y)
+    else: 
+        resistance_x, resistance_y = lift_acceleration(vx, vy, v, y)
+    # TODO: calcular as 2 acelarações em conjunto?? ou tem de ser separado??
+    total_ax = dx + gx + resistance_x
+    total_ay = dy + gy + resistance_y
+    return total_ax, total_ay
 
 
 def run_entry_simulation(altitude_0, angle_0, v_0):
@@ -101,8 +123,9 @@ def run_entry_simulation(altitude_0, angle_0, v_0):
         '''TODO: Runge-Kutta methods for better accuracy'''
         '''TODO: analitic methods...'''
         # total acceleration
-        ax, ay = total_acceleration(x, y, vx, vy)
-        
+        deployment = ((y - RADIUS_EARTH) <= ALTITUDE_BOUNDARY) and (np.sqrt(vx**2 + vy**2) <= VELOCITY_BOUNDARY)
+        ax, ay = total_acceleration(x, y, vx, vy, deployment)
+
         # velocity 
         vx += ax * dt
         vy += ay * dt
@@ -130,6 +153,7 @@ def plot_air_density(f):
     plt.ylabel('air_density')
     plt.show()
 
+
 def plot_path(path_x, path_y):
     '''plot the path of the capsule'''
     fig, ax = plt.subplots(figsize=(12, 8))
@@ -139,9 +163,11 @@ def plot_path(path_x, path_y):
     ax.legend()
     plt.show()
 
+
 def main():
     path_x, path_y = run_entry_simulation(altitude_0, angle_0, v_0)
     plot_path(path_x, path_y)
+
 
 if __name__ == "__main__":
     main()
